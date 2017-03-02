0d8530c56bf8d5bc4b51dc9dcf81fd31194a0087
stevenkellow <steven.kellow@gmail.com>
2017-03-02 09:23:03 +0000

    Work auth into class

--- functions.php
+++ functions.php
@@ -8,23 +8,25 @@
 
 /* --------------------------------------------------------------------- */
 if (!function_exists('mc_subscribe')) {
-function mc_subscribe(){
+function mc_subscribe( $mailchimp_auth, $userdata ){
 // Subscribe
 
-	$url = 'https://' . $server . '.api.mailchimp.com/3.0/lists/' . $list_id . '/members/';
+	$url = 'https://' . $mailchimp_auth->server . '.api.mailchimp.com/3.0/lists/' . $mailchimp_auth->list_id . '/members/';
 	$rest = 'POST';
 
 	$first_name = $userdata->first_name;
 	$last_name = $userdata->last_name;
+    $user_email = $userdata->user_email;
 
 	// Get the 
-	$content= array("email_address" => $user_email, "status" => "subscribed", "merge_fields" => array( "FNAME" => $first_name, "LNAME" => $last_name ) );
+	$content= array('email_address' => $user_email, 'status' => 'subscribed', 'merge_fields' => array( 'FNAME' => $first_name, 'LNAME' => $last_name ) );
 
 	$input = json_encode( $content );
 
-	// {"email_address": "urist.mcvankab@freddiesjokes.com","status": "subscribed","merge_fields": {"FNAME": "Urist","LNAME": "McVankab"}}
+	// Sample data sent as $input
+    // {"email_address": "urist.mcvankab@freddiesjokes.com","status": "subscribed","merge_fields": {"FNAME": "Urist","LNAME": "McVankab"}}
 
-	$data = mailchimp_curl( $url, $user, $rest, $input );
+	$data = mailchimp_curl( $url, $mailchimp_auth->name, $rest, $input );
     
     if( $data !== 'error' ){
 
@@ -38,11 +40,11 @@ function mc_subscribe(){
         $time = strtotime( current_time( 'mysql' ) );
 
         // Maybe save to WordPress?
-        update_user_meta( $user_id, 'mailchimp_id', $mailchimp_id );
-        update_user_meta( $user_id, 'mailchimp_email', $mailchimp_email ); // Cause sometimes users can be awkward
-        update_user_meta( $user_id, 'mailchimp_status', $mailchimp_status );
-        update_user_meta( $user_id, 'mailchimp_interests', $mailchimp_interests );
-        update_user_meta( $user_id, 'mailchimp_update_time', $time ); // Store current timestamp in a user_meta as the last check
+        update_user_meta( $userdata->id, 'mailchimp_id', $mailchimp_id );
+        update_user_meta( $userdata->id, 'mailchimp_email', $mailchimp_email ); // Cause sometimes users can be awkward
+        update_user_meta( $userdata->id, 'mailchimp_status', $mailchimp_status );
+        update_user_meta( $userdata->id, 'mailchimp_interests', $mailchimp_interests );
+        update_user_meta( $userdata->id, 'mailchimp_update_time', $time ); // Store current timestamp in a user_meta as the last check
         
         return __('Successfully subscribed!', 'mailchimp-prefs');
         
@@ -56,22 +58,22 @@ function mc_subscribe(){
 }
 /* ------------------------------------------------------------------------------------ */
 if (!function_exists('mc_unsub')) {
-function mc_unsub(){
+function mc_unsub( $mailchimp_auth, $userdata ){
 	// Unsub
 
 	// Get the user's preferred MailChimp email
-    $user_email = get_user_meta( $user_id, 'mailchimp_email' );
+    $user_email = get_user_meta( $userdata->id, 'mailchimp_email' );
 
-	$url = 'https://' . $server . '.api.mailchimp.com/3.0/lists/' . $list_id . '/members/' . $user_email;
+	$url = 'https://' . $mailchimp_auth->server . '.api.mailchimp.com/3.0/lists/' . $mailchimp_auth->list_id . '/members/' . $user_email;
 	$rest = 'PATCH';
 
 	// Tell MailChimp we're unsubbing this email address
-	$content= array( "email_address" => $user_email, "status" => "unsubscribed");
+	$content= array( 'email_address' => $user_email, 'status' => 'unsubscribed');
 
 	$input = json_encode( $content ); // Make it so MailChimp understands
 
 	// Run the request and get the response
-	$data = mailchimp_curl( $url, $user, $rest, $input );
+	$data = mailchimp_curl( $url, $mailchimp_auth->name, $rest, $input );
 
     if( $data !== 'error' ){
 
@@ -82,8 +84,8 @@ function mc_unsub(){
         $time = strtotime( current_time( 'mysql' ) );
         
         // Maybe save to WordPress?
-        update_user_meta( $user_id, 'mailchimp_status', $mailchimp_status );
-        update_user_meta( $user_id, 'mailchimp_update_time', $time ); // Store current timestamp in a user_meta as the last check
+        update_user_meta( $userdata->id, 'mailchimp_status', $mailchimp_status );
+        update_user_meta( $userdata->id, 'mailchimp_update_time', $time ); // Store current timestamp in a user_meta as the last check
         
 
         return __('Successfully unsubscribed', 'mailchimp-prefs');
@@ -98,24 +100,24 @@ function mc_unsub(){
 }
 /* ------------------------------------------------------------------------------------ */
 if (!function_exists('mc_update')) {
-function mc_update(){
+function mc_update( $mailchimp_auth, $userdata ){
 	// Add/Update interests
 
 	// Get the user's preferred MailChimp email
-    $user_email = get_user_meta( $user_id, 'mailchimp_email' );
+    $user_email = get_user_meta( $userdata->id, 'mailchimp_email' );
 
-	$url = 'https://' . $server . '.api.mailchimp.com/3.0/lists/' . $list_id . '/members/' . $user_email;
+	$url = 'https://' . $mailchimp_auth->server . '.api.mailchimp.com/3.0/lists/' . $mailchimp_auth->list_id . '/members/' . $user_email;
 	$rest = 'PATCH';
 
-	$interests = get_user_meta( $user_id, $mailchimp_interests );
+	$interests = get_user_meta( $userdata->id, $mailchimp_interests );
 
 	// Set up the response to tsend to MailChimp
-	$content = array( "email_address" => $user_email, "interests" => $interests);
+	$content = array( 'email_address' => $user_email, 'interests' => $interests);
 
 	$input = json_encode( $content ); // Make it so MailChimp understands
 	
 	// Run the request and get the response
-	$data = mailchimp_curl( $url, $user, $rest, $input );
+	$data = mailchimp_curl( $url, $mailchimp_auth->name, $rest, $input );
 
     if( $data !== 'error'){
     
@@ -126,8 +128,8 @@ function mc_update(){
         $time = strtotime( current_time( 'mysql' ) );
 
         // Maybe save to WordPress?
-        update_user_meta( $user_id, 'mailchimp_interests', $mailchimp_interests );
-        update_user_meta( $user_id, 'mailchimp_update_time', $time ); // Store current timestamp in a user_meta as the last check
+        update_user_meta( $userdata->id, 'mailchimp_interests', $mailchimp_interests );
+        update_user_meta( $userdata->id, 'mailchimp_update_time', $time ); // Store current timestamp in a user_meta as the last check
         
         return __('Details successfully updated.', 'mailchimp-prefs');
         
@@ -141,16 +143,16 @@ function mc_update(){
 }
 /*-------------------------------------------------------------------------------*/
 if (!function_exists('mc_check')) {
-function mc_check(){
+function mc_check( $mailchimp_auth, $userdata ){
 	
 	// Check subscriber status (in case someone unsubs)
     
     // Get original status
-    $status = get_user_meta( $user_id, 'mailchimp_status' );
+    $status = get_user_meta( $userdata->id, 'mailchimp_status' );
     
     // Get current time and compare to anything saved
     $time = strtotime( current_time( 'mysql' ) );
-    $last_check = strtotime( get_user_meta( $user_id, 'mailchimp_update_time' ) );
+    $last_check = strtotime( get_user_meta( $userdata->id, 'mailchimp_update_time' ) );
     
     $time_diff = $time - $last_check;
     
@@ -158,13 +160,13 @@ function mc_check(){
     if( $time_diff > 86400 ){
     
         // Get the user's preferred MailChimp email
-        $user_email = get_user_meta( $user_id, 'mailchimp_email' );
+        $user_email = get_user_meta( $userdata->id, 'mailchimp_email' );
 
-        $url = 'https://' . $server . '.api.mailchimp.com/3.0/lists/' . $list_id . '/members/' . $user_email;
+        $url = 'https://' . $mailchimp_auth->server . '.api.mailchimp.com/3.0/lists/' . $mailchimp_auth->list_id . '/members/' . $user_email;
         $rest = 'GET';
 
         // Run the checker
-        $data = mailchimp_curl( $url, $user, $rest );
+        $data = mailchimp_curl( $url, $mailchimp_auth->name, $rest );
 
         /*
         *
@@ -184,16 +186,16 @@ function mc_check(){
 
 
             // Maybe save to WordPress?
-            update_user_meta( $user_id, 'mailchimp_status', 'subscribed' );
-            update_user_meta( $user_id, 'mailchimp_update_time', $time ); // Store current timestamp in a user_meta as the last check
-            update_user_meta( $user_id, 'mailchimp_interests', $mailchimp_interests );
+            update_user_meta( $userdata->id, 'mailchimp_status', 'subscribed' );
+            update_user_meta( $userdata->id, 'mailchimp_update_time', $time ); // Store current timestamp in a user_meta as the last check
+            update_user_meta( $userdata->id, 'mailchimp_interests', $mailchimp_interests );
             
             return __('Subscribed', 'mailchimp-prefs');
                 
                 
             } else {
                 
-                update_user_meta( $user_id, 'mailchimp_status', 'unsubscribed' );
+                update_user_meta( $userdata->id, 'mailchimp_status', 'unsubscribed' );
                 
                 return __('Unsubscribed', 'mailchimp-prefs');
                 
@@ -217,31 +219,22 @@ function mc_check(){
 }
 /*-------------------------------------------------------------------------------*/
 if (!function_exists('mc_register')) {
-function mc_register(){
+function mc_register( $mailchimp_auth, $userdata, $signup ){
     
     // Check incase the user's email is already on file, and if it is then let's avoid the function
     if( ! get_user_by( 'email', $email ) ){
-    
-        // Set up the userdata to create
-        $userdata = array(
-            'user_login'  =>  $username,
-            'user_email'  =>  $email,
-            'user_pass'   =>  $password,  // When creating an user, `user_pass` is expected.
-            'first_name'  =>  $first_name,
-            'last_name'   =>  $last_name
-        );
 
         // Create the user
         $user_id = wp_insert_user( $userdata ) ;
         
         // If user creation worked
-        if( $user_id ){
+        if( is_int( $user_id ) ){
             
             // If the user wanted to sign up to MailChimp
             if( $signup == 1 ){
                 
                 // Sign them up to MailChimp
-                $message = mc_subscribe();
+                $message = mc_subscribe( $mailchimp_auth, $userdata );
             
                 if( $message !== 'error' ){
 
